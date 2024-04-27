fc1e0bc53e9c7f38fb06afe0f2782b52bd5b5b74
Krurmi33 <2397429787@qq.com>
2024-04-27 22:22:15 +0800

    refactor: folder pic

---
+++ vis/task2/Plot_Corr_Matrix.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    correlation_matrix = pd.read_csv('../../data/task2/corr_G.csv')
+    correlation_matrix.set_index(correlation_matrix.columns[0], inplace=True)
+    correlation_matrix = correlation_matrix.reset_index(drop=True)
+    column_names = correlation_matrix.columns.tolist()
+    new_index = [None] * len(correlation_matrix)
+
+    for i, column_name in enumerate(column_names):
+        new_index[i] = column_name
+
+    correlation_matrix.index = new_index
+    correlation_matrix = correlation_matrix.abs()
+
+    plt.figure(figsize=(10, 8))
+    Heatmap = sns.heatmap(correlation_matrix, annot=True, cmap='Reds', vmin=0, vmax=1)
+    plt.title("Geography Info Correlation Matrix Heatmap")
+    plt.xticks(fontsize=6)
+    plt.yticks(fontsize=6)
+
+    filename = '..\\..\\pic\\task2\\2_Corr_Matrix_Heatmap.png'
+    plt.savefig(filename, bbox_inches='tight', dpi=300)
